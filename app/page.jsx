ee8df48fb46a9fda185ff0f430b5a00a5529552c
Haithem-Hammami <haithem192000@gmail.com>
2025-03-14 09:57:12 +0100

    init

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Page() {    return (
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                <ContextAlert />
+              
                 <h1 className="mb-0">Hello Haithem</h1>
             </section>
         </main>
